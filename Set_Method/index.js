38c24990c8faa406647a9804e4b72539ca6be5a6
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-22 10:46:59 +0530

    update

---
+++ Set_Method/index.js
@@ -0,0 +1,51 @@
+// setTimeout(()=>{
+//     console.log("hey");
+// },1000);
+
+// setTimeout(() => {
+//     console.log("heyy")
+// }, 1000);
+
+
+var counter = 1 ; 
+const myid = setInterval(() => {
+    console.log(counter )
+    if(counter == 5){
+        clearInterval(myid)
+    }
+    else{
+        counter++
+    }
+}, 1000);
+
+// let counter = 1;
+
+// const myInterval = setInterval(() => {
+//     console.log(counter);
+    
+//     if (counter === 5) {
+//         clearInterval(myInterval); // Stop when 5 is printed
+//     }
+
+//     counter++;
+// }, 1000);
+
+// var counter = 1;
+// const myid = setInterval(() => {
+//   console.log(counter, myid);
+//   if (counter == 5) {
+//     clearInterval(myid);
+//   } else {
+//     counter++;
+//   }
+// }, 2000);
+
+// var counter = 1;
+// const myid = setInterval(() => {
+//     console.log(counter, myid); // this runs even when counter == 5
+//     if (counter == 5) {
+//         clearInterval(myid);
+//     } else {
+//         counter++;
+//     }
+// }, 1000);
