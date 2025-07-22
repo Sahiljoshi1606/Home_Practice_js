d20b87f9a5dc16e72bfbc4599c00bdf6da088fe2
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-22 10:24:33 +0530

    update

--- Object.js/index.js
+++ Object.js/index.js
@@ -1,23 +1,54 @@
-var num = [1,1,2,3,3,2,3,4,5,4,4,4,5,5,5,5,5,5,5];
-var obj = {};
-for (let i = 0 ; i < num.length ; i++){
-  if (obj [num[i]]){
-    obj [num[i]]++
-  }
-  else{
-    obj [num[i]]=1
-  }
-    }
-     console.log(obj)  
+// var num = [1,1,2,3,3,2,3,4,5,4,4,4,5,5,5,5,5,5,5];
+// var obj = {};
+// for (let i = 0 ; i < num.length ; i++){
+//   if (obj [num[i]]){
+//     obj [num[i]]++
+//   }
+//   else{
+//     obj [num[i]]=1
+//   }
+//     }
+//      console.log(obj)  
 
 
-     let max = 0 ;
-     let value = null;
-for (let key in obj){
-    if(obj[key] > max){
-        value = obj[key];
-        max = obj[key];
-        console.log (max , value)
-    }
-}
+//      let max = 0 ;
+//      let value = null;
+// for (let key in obj){
+//     if(obj[key] > max){
+//         value = obj[key];
+//         max = obj[key];
+//         console.log (max , value)
+//     }
+// }
+// // console.log(value)
+
+
+// var num = [1,1,2,3,3,2,3,4,5,4,4,4,5,5,5,5,5,5,5]; 
+
+// function frequency (num){
+// var obj = {};
+// for ( i = 0 ; i < num.length ; i++){
+//   if( obj [num[i]]){
+//     obj [num[i]]++
+//   }
+//   else{
+//       obj [num[i]] = 1
+//   }
+  
+// }
+// console.log(obj)
+
+// let max  = 0 ;
+// value = null ; 
+// for( let key in obj){
+//   if(obj[key] > max){
+//     value = key ; 
+//     max = obj[key]
+
+//   }
+// }
 // console.log(value)
+
+// }
+// frequency (num)
+
