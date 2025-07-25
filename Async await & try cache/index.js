6e3a306e2f8d66e747b0afa22f2e7668d941ef65
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-07-25 16:37:55 +0530

    update

--- Async await & try cache/index.js
+++ Async await & try cache/index.js
@@ -1,17 +1,26 @@
-function feachdata (){
-    return new Promise ((resolve , reject)=>{
-        setTimeout(() => {
-           reject ("product")
-        }, 5000);
-    })
-}
- async function data (){
-   try{
-     const result = await feachdata()
-    console.log(result)
-   }
-   catch(error){
-    console.log(error)
-   }
-}
-data()
+// function feachdata (){
+//     return new Promise ((resolve , reject)=>{
+//         setTimeout(() => {
+//            reject ("product")
+//         }, 5000);
+//     })
+// }
+//  async function data (){
+//    try{
+//      const result = await feachdata()
+//     console.log(result)
+//    }
+//    catch(error){
+//     console.log(error)
+//    }
+// }
+// data()
+
+// const a = 10
+// try{
+//   a = 20 ;
+ 
+// }
+// catch(error){
+//   console.log(error ,  "error find one")
+// }
